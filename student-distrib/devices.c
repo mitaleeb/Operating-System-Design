7beffd4f792bc48d75660d00e441d92c3d307e35
snehak3 <snehak3@illinois.edu>
2019-03-09 02:58:19 -0600

    Update devices.c

--- student-distrib/devices.c
+++ student-distrib/devices.c
@@ -10,17 +10,48 @@
 #include "idt.h"
 #include "devices.h"
 
+
+static uint8_t keyboard_output[4][128] = {
+	/* regular */
+	{'\0', '\0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=',
+	 '\0', '\0', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']',
+	 '\0', '\0', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\', '`',
+	 '\0', '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '\0', '*',
+	 '\0', ' ', '\0'},
+	/* shift */
+	{'\0', '\0', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+',
+	 '\0', '\0', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}',
+	 '\0', '\0', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L' , ':', '"', '~',
+	 '\0', '|', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '\0', '*',
+	 '\0', ' ', '\0'},
+	/* caps lock */
+	{'\0', '\0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=',
+	 '\0', '\0', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']',
+	 '\0', '\0', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L' , ';', '\'', '`',
+	 '\0', '\\', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', '\0', '*',
+	 '\0', ' ', '\0'},
+	/* caps lock and shift */
+	{'\0', '\0', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+',
+	 '\0', '\0', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '{', '}',
+	 '\0', '\0', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l' , ':', '"', '~',
+	 '\0', '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '<', '>', '?', '\0', '*',
+	 '\0', ' ', '\0'}
+};
+
+
+
 /* Function to initialize keyboard device */
 void init_keyboard() {
+    
     /* keyboard is on IR1 of master PIC */
     unsigned int irq_keyboard = 1;
-    
     enable_irq(irq_keyboard);
 }
 
 /* Function to handle keyboard interrupt */
 unsigned char handle_keyboard_interrupt() {
-    /* first, need to set 0x28 vector in the IDT to
+    
+    /* 0x28 vector in the IDT was set to
      * handle_keyboard_interrupt */
     
     unsigned char c = 0x00;
@@ -29,6 +60,13 @@ unsigned char handle_keyboard_interrupt() {
         /* read from 0x60 = data port from keyboard controller */
         c = inb(0x60);
     }
+    
+    switch (c) {
+        // based on which character was input
+    }
+    
+    unsigned int irq_keyboard = 1;
+    send_eoi(irq_keyboard);
 }
 
 /* Function to initialize rtc */
@@ -55,10 +93,6 @@ extern void init_rtc() {
 
 /* Function to handle rtc interrupt */
 extern void handle_rtc_interrupt() {
-    
-    /* send end of interrupt */
-    unsigned int irq_rtc = 8;
-	send_eoi(irq_rtc);
 	
     /* clear interrupts */
     // cli();
@@ -66,6 +100,10 @@ extern void handle_rtc_interrupt() {
 	/* read from status register C */
 	outb(0x8C, 0x70);
 	inb(0x71);
+	
+	/* send end of interrupt */
+    unsigned int irq_rtc = 8;
+	send_eoi(irq_rtc);
 	
 	/* set interrupts */
     // sti();
