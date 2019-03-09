b0fc44ac0cb42504be63b622f0b861458115a14f
snehak3 <snehak3@illinois.edu>
2019-03-08 23:29:39 -0600

    Update idt.c

--- student-distrib/idt.c
+++ student-distrib/idt.c
@@ -9,4 +9,40 @@
 
 void populate_idt(idt_desc_t* idt) {
 
+	/* load IDT */
+	lidt(idt_desc_ptr);
+	
+    int i;								
+	for(i=0; i<NUM_VEC; i++) {
+		
+		idt[i].present = 0x01;      /* set present bit to 1 */
+		idt[i].dpl = 0x00;          /* set descriptor privilege level to kernel */
+		idt[i].size = 0x01;
+		idt[i].seg_selector = KERNEL_CS;
+
+		/* set reserved bits depending on vector number */				
+		idt[i].reserved0 = 0x00;
+		idt[i].reserved1 = 0x01;
+		idt[i].reserved2 = 0x01;
+		if (i < 32) {
+		    idt[i].reserved3 = 0x01;
+		} else {
+		    idt[i].reserved3 = 0x00;
+		}
+		idt[i].reserved4 = 0x00;
+
+		
+		/* if vector > 32 interrupt, general exception */
+		if(i >= 32) {
+			SET_IDT_ENTRY(idt[i], general_exception);
+		}
+		
+		if(SYSCALL_INT == i) {
+			idt[i].dpl = 0x3;
+		}
+		
+	}
+
+	/* set IDT entries */
+
 }
