0a8dcedd2fc87b5a06e6e24466f1907f6700e2cc
snehak3 <snehak3@illinois.edu>
2019-03-08 20:50:50 -0600

    Update keyboard.h

--- student-distrib/keyboard.h
+++ student-distrib/keyboard.h
@@ -1,5 +1,19 @@
+/**
+ * keyboard.h
+ *
+ * h file that holds a lot of definitions of functions, including all the
+ * functions in keyboard.c
+ */
+
+#include "x86_desc.h"
+#include "idt.h"
+
+/**
+ * Function Declarations
+ */
+ 
 /* Function to initialize keyboard device */
-void init_keyboard();
+extern void init_keyboard();
 
 /* Function to handle keyboard interrupt */
-void handle_keyboard_interrupt();
+extern void handle_keyboard_interrupt();
