9bff8b27642823731e28acb54ac0caaa9dd53a69
snehak3 <snehak3@illinois.edu>
2019-03-09 03:15:14 -0600

    Update idt.h

--- student-distrib/idt.h
+++ student-distrib/idt.h
@@ -16,23 +16,56 @@ extern void populate_idt(idt_desc_t* idt);
 reserved vectors. We can use an "&" to get the address of this when we use them
 to set IDT value */
 
-/**
- * idt_de()
- *
- * Divide by zero error. Interrupt handler for interrupt vector 0x00. 
- */
+/* idt_de() - Divide Error. Vector 0x00. */
 extern void idt_de();
 
+/* idt_db() - RESERVED. Vector 0x01. */
 extern void idt_db();
 
+/* idt_nmi() - Nonmaskable External Interrupt. Vector 0x02. */
 extern void idt_nmi();
 
+/* idt_bp() - Breakpoint. Vector 0x03. */
 extern void idt_bp();
 
+/* idt_of() - Overflow. Vector 0x04. */
 extern void idt_of();
 
+/* idt_br() - BOUND range exceeded. Vector 0x05. */
 extern void idt_br();
 
+/* idt_ud() - Undefined Opcode. Vector 0x06. */
 extern void idt_ud();
 
-extern void idt_kk();
+/* idt_nm() - Device Not Available. Vector 0x07. */
+extern void idt_nm();
+
+/* idt_df() - Double Fault. Vector 0x08. */
+extern void idt_df();
+
+/* idt_ts() - Invalid TSS. Vector 10 (0x0A). */
+extern void idt_ts();
+
+/* idt_np() - Segment Not Present. Vector 11 (0x0B). */
+extern void idt_np();
+
+/* idt_ss() - Stack Segment Fault. Vector 12 (0x0C). */
+extern void idt_ss();
+
+/* idt_gp() - General Protection. Vector 13 (0x0D). */
+extern void idt_gp();
+
+/* idt_pf() - Page Fault. Vector 14 (0x0E). */
+extern void idt_pf();
+
+/* idt_mf() - Math Fault. Vector 16 (0x10). */
+extern void idt_mf();
+
+/* idt_ac() - Alignment Check. Vector 17 (0x11). */
+extern void idt_ac();
+
+/* idt_mc() - Machine Check. Vector 18 (0x12). */
+extern void idt_mc();
+
+/* idt_xf() - SIMD float exception. Vector 19 (0x13). */
+extern void idt_xf();
