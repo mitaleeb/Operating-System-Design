537860b1f70b1a22916fa03d85b730372956369f
Dean Biskup <dbiskup2@illinois.edu>
2019-04-23 19:58:03 -0500

    added a constants h file that we can use to clean up repeated defs

---
+++ student-distrib/constants.h
@@ -0,0 +1,17 @@
+/**
+ * constants.h - holds various constants that are generally global, such as
+ * megabyte/kilobyte sizes, etc.
+ */
+#ifndef _CONSTANTS_H
+#define _CONSTANTS_H
+
+/* defines for different byte sizes */
+#define FOUR_KB     0x00001000
+#define EIGHT_KB    0x00002000
+#define FOUR_MB     0x00400000
+#define EIGHT_MB    0x00800000
+#define TWELVE_MB   0x00C00000
+#define MB_128      0x08000000
+#define MB_132      0x08400000
+
+#endif
